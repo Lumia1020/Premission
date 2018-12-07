3be04a2e774d3b3406e7cd7076fb178e23dbcc40
talan1314 <talan1314@outlook.com>
2018-12-07 22:09:13 +0800

    tomcat7 与 jstl 冲突问题

--- Premission-Basic/src/main/java/com/linyi/controller/UserController.java
+++ Premission-Basic/src/main/java/com/linyi/controller/UserController.java
@@ -1,16 +1,32 @@
 package com.linyi.controller;
 
+import com.linyi.model.User;
 import com.linyi.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
+
     @Resource
     private UserService userService;
 
-    public void test1(){
-        this.userService.selectAll();
+    @RequestMapping("addUser")
+    @ResponseBody
+    public String addUserSubmit(User user) {
+        return userService.insert(user) > 0 ? "success" : "error";
     }
+
+    @RequestMapping("listUser")
+    public ModelAndView listUser() {
+         return new ModelAndView("user.jsp").addObject("users", this.userService.selectAll());
+    }
+
+
 }
